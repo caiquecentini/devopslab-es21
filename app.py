686a36ee55680dba94de7b300941444ba352945c
caiquecentini <92490040+caiquecentini@users.noreply.github.com>
2021-10-20 22:17:26 -0300

    Bug Removido

--- app.py
+++ app.py
@@ -11,16 +11,3 @@ def pagina_inicial():
 
 if __name__ == '__main__':
     app.run()
-
-@app.route('/bug')                                                                                                                                
-def bad():                                                                                                                                        
-    try:                                                                                                                                          
-        raise TypeError()                                                                                                                         
-    except TypeError as e:                                                                                                                        
-        print(e)                                                                                                                                  
-    except TypeError as e:                                                                                                                        
-        print("Duplicado, ou seja, nunca vai entrar aqui.")    
-
-
-if __name__ == '__main__':
-    app.run()
